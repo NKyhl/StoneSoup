f1a743ab2d0d295a837f82d3060746772708cadc
NKyhl <nolan@kyhl.com>
2023-10-30 18:09:42 -0400

    fix port

--- backend/app.py
+++ backend/app.py
@@ -136,4 +136,4 @@ def search_ingredient():
     pass
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5015, host='db8.cse.nd.edu')
+    app.run(debug=True, port=5036, host='db8.cse.nd.edu')
