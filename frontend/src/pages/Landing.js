ccee07527fa10eb00f161e0bc7df2fccb71b90c2
NKyhl <nolan@kyhl.com>
2023-12-01 15:30:17 -0500

    basic landing page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,9 +1,24 @@
-import Header from "./components/Header";
+import "./landing.css";
+import StoneSoupIcon from "../StoneSoupIcon.png";
+import { useNavigate } from 'react-router-dom';
+import CursorContextProvider from './components/CursorContextProvider';
+import Cursor from "./components/Cursor";
 
 function Landing(){
-    return (
-        <Header></Header>
-    );
+
+    const navigate = useNavigate();
+    const handleImgClick = () => {
+        navigate('/login');
+    }
+
+    return <>
+        <CursorContextProvider>
+        <Cursor />
+        <div className="large-image-container">
+            <img src={StoneSoupIcon} alt="Stone Soup" className="main-icon" onClick={handleImgClick}/>
+        </div>
+        </CursorContextProvider>
+    </>;
 }
 
 export default Landing;
