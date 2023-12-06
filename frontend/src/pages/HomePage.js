577232ab4b71eeaad535176cef00c9a7254b3ffb
NKyhl <nolan@kyhl.com>
2023-12-05 21:35:48 -0500

    colors, responsive calendar

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -55,18 +55,18 @@ function HomePage({ userData, setUserData }){
     return (
         <>
         <Header userData={userData} ></Header>
-        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", paddingTop: "20px", backgroundColor: "#ff7f50"}}>
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", paddingTop: "20px"}}>
             {/* <h1>Welcome {userData.name ? userData.name : "User"}!</h1> */}
-            <h1 style={{ color: "white"}}>My Meal Plan</h1>
+            <h1 style={{ color: "#f97316", margin: "0 0 15px 0"}}>Plan Your Week</h1>
         </div>
         <main>
             <WeekCalendar userData={userData} drag={drag} setDrag={setDrag} weekPlan={weekPlan} setWeekPlan={setWeekplan}></WeekCalendar>
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "20px"}}>
-                <h1 style={{ color: "white"}}>Recommendations</h1>
+                <h1 style={{ color: "#f97316"}}>Find Recommendations</h1>
             </div>
             <Recommendations setDrag={setDrag} weekPlan={weekPlan} userData={userData}></Recommendations>
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "20px"}}>
-                <h1 style={{ color: "white"}}>Search For Meals</h1>
+                <h1 style={{ color: "#f97316"}}>Search For Meals</h1>
             </div>
             <Search setDrag={setDrag}></Search>
         </main>
